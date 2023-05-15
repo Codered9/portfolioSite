90a70f6ee5521754897c39130075a3a7e46f9622
Sudhanshu Srivastava <sudhanshusri23@gmail.com>
2023-05-15 15:52:37 +0530

    Resume Update

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -5,10 +5,11 @@ const Resume = () => {
     <div className="min-h-[93vh] pt-24 px-8 md:px-24" id="resume">
         <h2 className="text-4xl"><span className='text-br-red font-black'>$</span>Resume</h2>
         <div className='mt-4'>
-        <iframe src="https://drive.google.com/file/d/1bzR5GWnc8u_qm6RLwPxDpAPi1ZuRtT8j/preview" allow="autoplay" className='h-[70vh] w-full md:h-[85vh] md:w-[70vh] mx-auto'></iframe>
+        <iframe src="https://drive.google.com/file/d/17u7rEwW86JhcYwDnMqy06eH1hckEzLec/preview" allow="autoplay" className='h-[70vh] w-full md:h-[85vh] md:w-[70vh] mx-auto'></iframe>
         </div>
     </div>
   )
 }
+// https://drive.google.com/file/d/17u7rEwW86JhcYwDnMqy06eH1hckEzLec/view?usp=share_link
 
 export default Resume
